4ca3652b8133d43baec2f13e5a656202ecba60f8
Guillaume Hernandez <guillaume.hernandez.1@gmail.com>
2024-05-26 02:22:46 +0200

    Add UI pluggins - BUG ON BUFFERLINE TO BE FIXED

--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -1,7 +1,26 @@
 local package = require('core.pack').package
 local config = require('modules.ui.config')
 
+   -- Colorsheme
 package({ "catppuccin/nvim", 
    name = "catppuccin", 
    priority = 1000,
    config = config.catppuccin}) -- Color scheme "Catpuccin"
+
+
+   -- Lua Line
+package({ "nvim-lualine/lualine.nvim", 
+   dependencies = "nvim-tree/nvim-web-devicons",
+   config = config.lualine})
+
+   -- Bufferline
+package({ 'akinsho/bufferline.nvim', 
+   tag = 'v4.5.3',
+
+   dependencies = 'nvim-tree/nvim-web-devicons',
+   config = config.bufferline})
+
+   -- file explorer
+package({ "nvim-tree/nvim-tree.lua", 
+   lazy = true, 
+   dependencies = 'nvim-tree/nvim-web-devicons'})
